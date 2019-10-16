f17073b4f8230e13366a586b4ca078e5f3d5d3f1
Amin Mehrzad <amin.mehrzad@gmail.com>
2019-10-16 10:53:29 -0700

    auth page bootstraped

--- routes/auth.js
+++ routes/auth.js
@@ -3,46 +3,61 @@ router = express.Router();
 BigCommerce = require('node-bigcommerce');
 
 const bigCommerce = new BigCommerce({
-clientId: 'ne9vzqqdj8x8rxni55z6j2c361zlfdd',
-secret: 'e05b823ba6560820a8f77a451f214dba3a77534e5feb295de826b8ae7ee17924',
-callback: 'https://fc64c7de.ngrok.io/auth',
-responseType: 'json'
+    clientId: 'ne9vzqqdj8x8rxni55z6j2c361zlfdd',
+    secret: 'e05b823ba6560820a8f77a451f214dba3a77534e5feb295de826b8ae7ee17924',
+    callback: 'https://fc64c7de.ngrok.io/auth',
+    responseType: 'json'
 });
 
 router.get('/', (req, res, next) => {
     var db = req.db;
-    var findKey;
-    var myPromise = new Promise(function(resolve, reject) {
-            
-    
+    var websiteKey;
+    var accessToken;
+    var myPromise = function (websiteKey, accessToken) {
+        return   new Promise(function (resolve, reject) {
             //do something, fetch something....
             //you guessed it, mongo queries go here.
-            db.collection('usercollection').find({"websiteKey": findKey })
-            .then(function(res){
-                resolve(res);
-                console.log("xxx");
-            })
+            db.collection('usercollection').update({ "websiteKey": websiteKey }, {
+                $set: {
+                    "publicKey": "publicKeyy",
+                    "secretKey": "secretKey",
+                    "websiteKey": websiteKey,
+                    "accessToken": accessToken
+                }
+            }, { upsert: true })
+                .then(function (res) {
+                    resolve(res);
+                    console.log("xxx");
+                })
             //I can continue to process my result inside my promise
-             if(false) {reject('aasdasdas');}
+            if (false) { reject('aasdasdas'); }
 
         });
+    }
+    bigCommerce.authorize(req.query)
+        //.then(data => console.log(data['access_token']))
+        .then(function (data) {
 
-bigCommerce.authorize(req.query)
-//.then(data => console.log(data['access_token']))
-.then(function(data){
-            console.log("asdasdasdasd");
-            findKey = "global.websiteKey";
-        myPromise.then(function(value){console.log(data);
-    console.log(value)});
+            console.log(data['user']['username']);
+            var contextArray = data['context'].split("/");
+            console.log(contextArray[1]);
+            console.log("+++++++++++++++++++++");
 
-   
-})
-.then(data => res.render('auth', { title: 'Validage App Installed Successfully!' }))
-.catch(next);
+            websiteKey = contextArray[1];
+            accessToken = data['access_token'];
+            myPromise(websiteKey,accessToken).then(function (value) {
+                console.log(data);
+                console.log(value)
+                res.render('auth', { title: ['Valid','Age'] })
+            });
+
+
+        })
+        //.then(data => )
+        .catch(next);
 });
 module.exports = router;
 
 
 
-  
- 
+
